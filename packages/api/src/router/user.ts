7f781510127751290ac76de73c96dd8f76771b00
Alex Hu <alexhu@Alexs-MacBook-Pro.local>
2023-03-16 23:55:45 +1030

    Add: added trpc-panel for api calls without UI. Connected to railway database

--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -1,24 +1,39 @@
 import { z } from "zod";
-import { createTaskInputSchema } from "../schemas/createTasksInputSchema";
 
+import { createTaskInputSchema } from "../schemas/createTasksInputSchema";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
   getUser: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.user.findUnique({where : {id: ctx.session?.user.id}, include : {oversees  : true, tasks:true}});
+    return ctx.prisma.user.findUnique({
+      where: { id: ctx.session?.user.id },
+      include: { oversees: true, tasks: true },
+    });
   }),
   createTask: publicProcedure
-    .input(
-      createTaskInputSchema
-    )
+    .input(createTaskInputSchema)
     .mutation(({ ctx, input }) => {
-      return ctx.prisma.tasks.create({ data: input });
+      return ctx.prisma.task.create({
+        data: input,
+      });
     }),
   updateTaskStatus: publicProcedure
-    .input(z.object({
-      id :z.string(),
-      status : z.enum(["REQUESTED", "PENDING", "COMPLETE","APPROVED","RETURNED"]),
-    })).mutation(({ ctx, input }) => {
-    return ctx.prisma.post.update({ where: { id: input.id }, data : {status : input.status} });
-  }),
+    .input(
+      z.object({
+        id: z.string(),
+        status: z.enum([
+          "REQUESTED",
+          "PENDING",
+          "COMPLETE",
+          "APPROVED",
+          "RETURNED",
+        ]),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.task.update({
+        where: { id: input.id },
+        data: { status: input.status },
+      });
+    }),
 });
--- packages/api/src/schemas/createTasksInputSchema.ts
+++ packages/api/src/schemas/createTasksInputSchema.ts
@@ -1,15 +1,14 @@
 import { z } from "zod";
 
-export const createTaskInputSchema = 
-    z.object({
-        createrId : z.string().min(1),
-        overseerUsername: z.string().min(1),
-        title: z.string().min(1),
-        description: z.string().min(1),
-        createdAt: z.string().min(1),
-        expireAt: z.string().min(1),
-        reward: z.number(),
-        status : z.enum(["REQUESTED", "PENDING", "COMPLETE","APPROVED","RETURNED"]),
-    })
-  
-export type createTaskInput = z.TypeOf<typeof createTaskInputSchema>
+export const createTaskInputSchema = z.object({
+  creatorId: z.string().min(1),
+  overseerUsername: z.string().min(1),
+  title: z.string().min(1),
+  description: z.string().min(1),
+  createdAt: z.string().min(1),
+  expireAt: z.string().min(1),
+  reward: z.number(),
+  status: z.enum(["REQUESTED", "PENDING", "COMPLETE", "APPROVED", "RETURNED"]),
+});
+
+export type createTaskInput = z.TypeOf<typeof createTaskInputSchema>;
